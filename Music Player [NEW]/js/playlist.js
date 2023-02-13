3f0ab6e1cadbf84d3c08499774db1ff67124e9a6
CHANDAN KUMAR MALLICK <chandansoumya28@gmail.com>
2023-02-13 20:37:29 +0530

    Updated

---
+++ Music Player [NEW]/js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+      name: "6 Shots - NEFFEX",
+      artist: "NEFFEX",
+      img: "img1",
+      audio: "music1"
+    },
+    {
+      name: "No Mercy - TrackTribe",
+      artist: "TrackTribe",
+      img: "img2",
+      audio: "music2"
+    },
+    {
+      name: "Slipping Away - Dyalla",
+      artist: "Dyalla",
+      img: "img3",
+      audio: "music3"
+    },
+    {
+      name: "Desert Planet - Quincas",
+      artist: "Quincas",
+      img: "img4",
+      audio: "music4"
+    }
+  ]
